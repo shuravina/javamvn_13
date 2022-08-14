34059270a047ba06f1ccbf18e0a1c2cd4ead6552
Galina Shuravina <shuravinags@mail.ru>
2022-08-14 13:47:13 +0700

    firstversion

---
+++ src/main/java/Manager.java
@@ -0,0 +1,50 @@
+public class Manager {
+
+    private ProductRepository repository;
+
+    public Manager(ProductRepository repository) {
+
+        this.repository = repository;
+    }
+
+//    public Manager() {
+
+//    }
+
+    public void add(Product product) {
+
+        repository.add(product);
+    }
+
+    public void removeById(int id) {
+        repository.remove(id);
+    }
+
+    public Product[] findAll() {
+        return repository.findAll();
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+            }
+        }
+        return result;
+    }
+
+    // метод определения соответствия товара product запросу search
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
